3d22304d4120031aa803cd7da4a6a139b83e0606
Vijay Thirugnanam <vijayst@Vijays-MacBook-Air.local>
2017-11-24 22:38:24 +0530

    User controller

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+class UserController {
+    static signup(req, res) {
+
+    }
+
+    static login(req, res) {
+
+    }
+
+    static logout(req, res) {
+
+    }
+
+    static user(req, res) {
+
+    }
+
+    static remove(req, res) {
+        
+    }
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const BirdController = require('./controllers/birdController');
+const UserController = require('./controllers/userController');
 const PORT = 3000;
 
 mongoose.Promise = global.Promise;
@@ -9,10 +10,17 @@ mongoose.connect('mongodb://localhost/birddb', { useMongoClient: true });
 
 const app = express();
 app.use(bodyParser.json());
+
 app.post('/api/birds', BirdController.create);
 app.put('/api/birds/:id', BirdController.update);
 app.delete('/api/birds/:id', BirdController.remove);
 app.get('/api/birds', BirdController.index);
+
+app.post('/api/signup', UserController.signup);
+app.post('/api/login', UserController.login);
+app.post('/api/logout', UserController.logout);
+app.get('/api/user', UserController.index);
+app.delete('/api/user/:id', UserController.remove);
 
 app.listen(PORT, () => {
     console.log(`listening at ${PORT}`);
