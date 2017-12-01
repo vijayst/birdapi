a920ea8582a20de8faa1a24ac279f6dc6ec1ecea
vijayst <vijayst@gmail.com>
2017-12-01 11:00:03 +0530

    validations to bird controller

--- controllers/birdController.js
+++ controllers/birdController.js
@@ -1,41 +1,50 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
 const Bird = require('../models/bird');
 
 class BirdController {
-    static create(req, res) {
+    static create(req, res, next) {
         Bird.create(req.body)
             .then(bird => {
                 res.send(bird);
-            });
+            })
+            .catch(next);
     }
 
-    static update(req, res) {
+    static update(req, res, next) {
         const { id } = req.params;
+        assert.ok(mongoose.Types.ObjectId.isValid(id), 'Id is not valid!');
         Bird.findByIdAndUpdate(id, req.body)
             .then(() => Bird.findById(id))
             .then(bird => {
                 res.send(bird);
-            });
+            })
+            .catch(next);
     }
 
-    static remove(req, res) {
+    static remove(req, res, next) {
         const { id } = req.params;
+        assert.ok(mongoose.Types.ObjectId.isValid(id), 'Id is not valid!');
         Bird.findByIdAndRemove(id)
-            .then(() => res.send({ id }));
+            .then(() => res.send({ id }))
+            .catch(next);
     }
 
-    static index(req, res) {
+    static index(req, res, next) {
         const { offset, limit } = req.query;
         Promise.all([
             Bird.find({})
                 .skip(offset)
                 .limit(limit),
             Bird.count()
-        ]).then(([birds, count]) => {
-            res.send({
-                birds,
-                count
-            });
-        });
+        ])
+            .then(([birds, count]) => {
+                res.send({
+                    birds,
+                    count
+                });
+            })
+            .catch(next);
     }
 }
 
